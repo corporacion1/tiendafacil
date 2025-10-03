4c94ff9fb5821c5f6aea4c46ca3dc52399c50a4c
Jorge Negrete <corporacion1@gmail.com>
2025-10-03 03:11:28 +0000

    Try fixing this error: `Runtime Error: Error: PackagePlus is not defined

--- src/app/purchases/page.tsx
+++ src/app/purchases/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from "react";
-import { PlusCircle, Trash2, X } from "lucide-react";
+import { PackagePlus, PlusCircle, Trash2, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
