a882a4cad1702bdc8c7eb9cf4474f016e30aefaf
Jorge Negrete <corporacion1@gmail.com>
2025-10-04 03:11:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5901149790-b8cda",
+  "appId": "1:571583090313:web:6758bb217de2a35fcb8058",
+  "apiKey": "AIzaSyDdmE63_Jfk_qLo4gmiA0vmXtUSdQ4rpeU",
+  "authDomain": "studio-5901149790-b8cda.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "571583090313"
+};
