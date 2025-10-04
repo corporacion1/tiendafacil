f572758011febc209c82abbbaba4d1a0ff9c9d5f
Jorge Negrete <corporacion1@gmail.com>
2025-10-04 05:43:57 +0000

    nada sigue igual no inicia

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -17,19 +17,11 @@ export function initializeFirebase() {
   };
 
   if (!getApps().length) {
-    let firebaseApp;
-    try {
-      firebaseApp = initializeApp();
-    } catch (e) {
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
+    const firebaseApp = initializeApp(firebaseConfig);
     return getSdks(firebaseApp);
+  } else {
+    return getSdks(getApp());
   }
-
-  return getSdks(getApp());
 }
 
 
