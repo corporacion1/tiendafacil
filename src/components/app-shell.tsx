2e3cb45ab47a56ddb03047c14272fbc602ede5a2
Jorge Negrete <corporacion1@gmail.com>
2025-10-05 01:21:47 +0000

    Try fixing this error: `Runtime Error: Error: useSettings must be used w

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -42,15 +42,17 @@ export function AppShell({ children }: { children: React.ReactNode }) {
   }
   
   return (
+    <AuthGuard>
       <div className="flex min-h-screen w-full flex-col bg-muted/40">
         <SiteSidebar />
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <SiteHeader />
           <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-            <AuthGuard>{children}</AuthGuard>
+            {children}
           </main>
           <Footer />
         </div>
       </div>
+    </AuthGuard>
   );
 }
