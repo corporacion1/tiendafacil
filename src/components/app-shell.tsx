c830fb3785d3e5764e7b20f4eb2f66a729214dc4
Jorge Negrete <corporacion1@gmail.com>
2025-10-05 01:19:11 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -8,12 +8,10 @@ import { Footer } from "@/components/footer";
 import { usePathname } from 'next/navigation';
 import { useEffect, useRef } from 'react';
 import { useSecurity } from "@/contexts/security-context";
-import { useUser } from '@/firebase';
 import { AuthGuard } from "./auth-guard";
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   const { isLocked, lockApp, hasPin, isMounted, isPinLoading } = useSecurity();
-  const { isUserLoading } = useUser();
   const pathname = usePathname();
   const previousPathname = useRef(pathname);
 
@@ -44,17 +42,15 @@ export function AppShell({ children }: { children: React.ReactNode }) {
   }
   
   return (
-    <AuthGuard>
       <div className="flex min-h-screen w-full flex-col bg-muted/40">
         <SiteSidebar />
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <SiteHeader />
           <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-            {children}
+            <AuthGuard>{children}</AuthGuard>
           </main>
           <Footer />
         </div>
       </div>
-    </AuthGuard>
   );
 }
