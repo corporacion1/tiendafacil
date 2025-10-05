aec35171319618dce48ec71e9fac5d4570c8848f
Jorge Negrete <corporacion1@gmail.com>
2025-10-05 01:01:40 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -9,7 +9,6 @@ import { usePathname, useRouter } from 'next/navigation';
 import { useEffect, useRef } from 'react';
 import { useSecurity } from "@/contexts/security-context";
 import { useUser } from '@/firebase';
-import { AuthGuard } from "./auth-guard";
 
 
 export function AppShell({ children }: { children: React.ReactNode }) {
@@ -40,10 +39,18 @@ export function AppShell({ children }: { children: React.ReactNode }) {
     return <>{children}</>;
   }
 
-  if (!isMounted) {
+  if (isUserLoading || !isMounted) {
      return (
         <div className="flex min-h-screen w-full items-center justify-center bg-background">
             <p>Cargando aplicación...</p>
+        </div>
+      );
+  }
+  
+  if (!user) {
+    return (
+        <div className="flex min-h-screen w-full items-center justify-center bg-background">
+            <p>Redirigiendo...</p>
         </div>
       );
   }
@@ -54,16 +61,14 @@ export function AppShell({ children }: { children: React.ReactNode }) {
   
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-        <AuthGuard>
-          <SiteSidebar />
-          <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-              <SiteHeader />
-              <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-                  {children}
-              </main>
-              <Footer />
-          </div>
-        </AuthGuard>
+        <SiteSidebar />
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+            <SiteHeader />
+            <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+                {children}
+            </main>
+            <Footer />
+        </div>
     </div>
   );
 }
