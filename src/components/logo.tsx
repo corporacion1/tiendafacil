4c47944e648d1a462c2ff6f6702963c9cb649fe2
Jorge Negrete <corporacion1@gmail.com>
2025-10-07 04:53:38 +0000

    ves como programar sin sentido es solo perdida de recursos y tiempo el l

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export const Logo = ({ className }: { className?: string }) => (
   <div className={cn("relative", className)}>
     <Image
-      src="https://i.imgur.com/kS40G2s.png"
+      src="https://www.dropbox.com/scl/fi/apl8e6ymm6wel3coa10a9/tienda_facil_logo.svg?rlkey=jn7j3dezgn1ovbl35vapk5ap5&raw=1"
       alt="Tienda Facil Logo"
       fill
       className="object-contain"
