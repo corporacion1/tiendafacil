7859ceddab1e71a41ab09688a21eb42554a991e1
Jorge Negrete <corporacion1@gmail.com>
2025-10-09 23:14:46 +0000

    puedes explicar porque sale ese error y como vamos a solucionarlo !!!

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,6 +2,5 @@ export const firebaseConfig = {
   "projectId": "studio-5901149790-b8cda",
   "appId": "1:571583090313:web:6758bb217de2a35fcb8058",
   "apiKey": "AIzaSyDdmE63_Jfk_qLo4gmiA0vmXtUSdQ4rpeU",
-  "authDomain": "studio-5901149790-b8cda.firebaseapp.com",
-  "messagingSenderId": "571583090313"
+  "authDomain": "studio-5901149790-b8cda.firebaseapp.com"
 };
