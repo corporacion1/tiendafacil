da60cb8aff2982578404af3a3ede376a63fad435
Jorge Negrete <corporacion1@gmail.com>
2025-10-13 05:44:55 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -50,7 +50,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'www.dropbox.com',
         port: '',
-        pathname: '/**',
+        pathname: '/**'
       }
     ],
   },
